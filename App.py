2db0fe14f1c630341dad905271481ff51681050d
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-15 18:32:34 -0600

    Added functions

--- App.py
+++ App.py
@@ -1,2 +1,18 @@
 price = 10
-print(price)
+
+# variable must be lower case - booleans must be capitalized
+is_published = False
+
+
+
+# birth_year = input('Birth year: ')
+# age = 2019 - birth_year
+# # print(age)
+# print(type(birth_year))
+
+weight = input('What is your weight: ')
+converted = weight * .45
+print(converted)
+
+
+
