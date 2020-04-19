518077b2b3cc7abd918a5b95a5077ff4e8eb15ff
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-18 19:49:49 -0600

    Added conditions.py

--- conditions.py
+++ conditions.py
@@ -1,11 +1,24 @@
 is_hot = False
 is_cold = False
 
-if is_hot:
-  print("it's a hot day")
-  print("Drink plenty of water!")
-elif is_cold:
-  print("It's a cold day")
-  print("Wear warm cloths")
+# if is_hot:
+#   print("it's a hot day")
+#   print("Drink plenty of water!")
+# elif is_cold:
+#   print("It's a cold day")
+#   print("Wear warm cloths")
+# else:
+#   print("enjoy your day")
+
+good_credit = False
+cost = 1000000
+down_payment = 0
+
+if good_credit:
+  down_payment = (cost * .10)
+  print(down_payment)
 else:
-  print("enjoy your day")
+  down_payment = (cost * .20)
+  print(down_payment)
+print(f"Down Payment: ${down_payment}")
+
