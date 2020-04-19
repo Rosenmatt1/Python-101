b643df93d551414c4ccac8e0eb7b7398f3e6923c
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-18 19:01:00 -0600

    Added a conditions.py conditinal statement

---
+++ conditions.py
@@ -0,0 +1,11 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+  print("it's a hot day")
+  print("Drink plenty of water!")
+elif is_cold:
+  print("It's a cold day")
+  print("Wear warm cloths")
+else:
+  print("enjoy your day")
