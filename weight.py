8d281b8890eb52c5036c4fa5d30bdef5acdcc09c
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-19 17:35:57 -0600

    Added a guessing app

--- weight.py
+++ weight.py
@@ -8,12 +8,34 @@
 
 # print(len(name))
 
-weight = int(input('What is your weight: '))
-unit = input("(L)bs or (K)g: ")
+# weight = int(input('What is your weight: '))
+# unit = input("(L)bs or (K)g: ")
 
-if unit == "K" or "k":
-  converted = weight * 2.2
-  print(f"You are {converted} kilos")
-elif unit == "L" or "l":
-  converted = weight / .45
-  print(f"You are {converted} pounds")
+# if unit == "K" or "k":
+#   converted = weight * 2.2
+#   print(f"You are {converted} kilos")
+# elif unit == "L" or "l":
+#   converted = weight / .45
+#   print(f"You are {converted} pounds")
+
+
+# i = 1
+
+# while i <= 5:
+#   print(i)
+#   i = i + 1
+# print("Done")
+
+
+secret_number = 7
+count = 0
+max_count = 3
+
+while count < max_count:
+  guess = int(input("Guess the number: "))
+  count = count + 1
+  if guess == secret_number:
+    print("You Guessed it")
+    break
+else:
+  print("Sorry ran out of guesses")
