ab39edc41f6ff9a99f61c10f11dc51dd5632af86
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-24 21:40:02 -0600

    Found greatest number function

---
+++ nested-loop.py
@@ -0,0 +1,32 @@
+# for x in range(4):
+#   for y in range(3):
+#     print(x, y)
+
+# numbers = [5, 2, 5, 2, 2]
+
+# for i in numbers:
+#   # for y in range(3):
+#   print('x' * i)
+
+
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mosh']
+numbers = [8, 9, 10, 2, 1]
+
+# starting at indx 1
+# print(names[1:2])
+
+max = numbers[0]
+
+for number in numbers:
+  if number > max:
+    max = number
+    print(max)
+
+
+
+
+
+
+
+
+
