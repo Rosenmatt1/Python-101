0d815a168cb300f1e0d1fa24b7a8506e84cfa017
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-24 20:56:52 -0600

    Added for loops

---
+++ for-loop.py
@@ -0,0 +1,18 @@
+# for item in 'Python':
+#   print(item)
+
+# for item in ['Python', 'Mosh', 'Sarah']:
+#   print(item)
+
+# for item in range(10):
+#       print(item)
+
+# for item in range(5, 10):
+#      print(item)
+
+total = 0
+prices = [10, 20, 30, 40]
+
+for item in prices:
+  total += total
+  print(total)
