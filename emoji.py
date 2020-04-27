a503e4de3de3e4296aa44f547065b0e6de259095
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-26 21:17:48 -0600

    Refactored emoji to function

--- emoji.py
+++ emoji.py
@@ -1,13 +1,18 @@
 message = input("> ")
-words = message.split(' ')
 
 # control + command + space bar
 emojis = {
-  ":)": "😃",
-  ":(": "😌"
+    ":)": "😃",
+    ":(": "😌"
 }
-output = ""
 
-for word in words:
-  output += emojis.get(word, word) + " "
-print(output)
+def run_emoji(message, emojis):
+  output = ""
+  words = message.split(' ')
+  for word in words:
+    output += emojis.get(word, word) + " "
+  return output
+
+
+result = run_emoji(message, emojis)
+print(result)
--- functions.py
+++ functions.py
@@ -3,3 +3,6 @@ def greet_user(name, age):
   print(f"His age is {age}")
 
 greet_user("John", 24)
+# # This way can not be in order
+# If both, Keyword Arguments must come after Positional Arguments
+greet_user(age=18, name="Tim")
