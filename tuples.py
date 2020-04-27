dc21682c0d9e909ee31173a0f3e151c9dd478848
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-26 19:48:03 -0600

    Added phone converter app

--- tuples.py
+++ tuples.py
@@ -3,3 +3,16 @@ coordinates = (1, 2, 3)
 
 # unpacks list of tuple into variables
 x, y, z = coordinates
+
+customer = {
+  "name": "John Smith",
+  "age": 30,
+  "is_verified": True
+}
+
+customer["name"] = "Jack Dong"
+customer["birthdate"] = "Jan 1 1980"
+
+# print(customer["name"])
+print(customer.get("name"))
+print(customer.get("birthdate"))
