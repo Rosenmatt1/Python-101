1e667f70a85b4e317dcfa8f5d0aacb8738379ce2
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-28 20:36:23 -0600

    Added a random.py file class

---
+++ random.py
@@ -0,0 +1,27 @@
+# import random
+
+# # for i in range(3):
+# #   print(random.random())
+
+# # for i in range(3):
+# #   print(random.randomint(10, 20))
+
+# members = ['John', 'Mary', 'Bob', 'Mosh']
+# leader = random.choice(members)
+# print(leader)
+
+
+# options = (1,2,3,4,5,6)
+
+class Dice:
+  def roll(self):
+    # value1 = dice.choice(options)
+    # value2 = dice.choice(options)
+    random1 = random.randint(1, 6)
+    random2 = random.randint(1, 6)
+    print(random1, random2)
+    return random1, random2
+    # () are not necesaary when returning a tuple
+
+roll1 = Dice()
+roll1.roll()
